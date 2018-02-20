6fe3cdb1691b21d5cb79cb8fbfe58fefc9d813a2
ernesto <enesto_p_r@hotmail.com>
2018-02-20 13:07:42 -0700

    Nueva pantalla

---
+++ OrdenamientoPesquero/OrdenamientoPesquero/Pantalla_registro_embarcacion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrdenamientoPesquero
+{
+    public partial class Pantalla_registro_embarcacion : Form
+    {
+        public Pantalla_registro_embarcacion()
+        {
+            InitializeComponent();
+        }
+    }
+}
