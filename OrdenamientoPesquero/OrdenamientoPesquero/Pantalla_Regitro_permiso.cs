b3d56ed625da55e8c45766b4d44ab941b381deee
ernesto <enesto_p_r@hotmail.com>
2018-02-21 12:44:39 -0700

    prp

---
+++ OrdenamientoPesquero/OrdenamientoPesquero/Pantalla_Regitro_permiso.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrdenamientoPesquero
+{
+    public partial class Pantalla_Regitro_permiso : Form
+    {
+        public Pantalla_Regitro_permiso()
+        {
+            InitializeComponent();
+        }
+    }
+}
