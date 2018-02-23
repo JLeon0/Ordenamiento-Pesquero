1fec4ccb594c743fe69615e24716c1983ef53290
ernesto <enesto_p_r@hotmail.com>
2018-02-23 12:13:14 -0700

    CAMBIO PARA EL MECOMAN

---
+++ OrdenamientoPesquero/OrdenamientoPesquero/Clases Entidad/Pescador.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdenamientoPesquero.Clases_Entidad
+{
+    class Pescador
+    {
+        string COD_REG { get; set; }
+        string NOMBRE { get; set; }
+        string AP_PAT { get; set; }
+        string AP_MAT { get; set; }
+        string CURP { get; set; }
+        string RFC { get; set; }
+
+        string ESCOLARIDAD { get; set; }
+        string TIP_SANG { get; set; }
+        string SEXO { get; set; }
+
+        string LUG_NACIMI { get; set; }
+        string FECH_NACIMI { get; set; }
+        string CALLENUM { get; set; }
+        string COLONIA { get; set; }
+        string LOCALIDAD { get; set; }
+        string MUNICIPIO { get; set; }
+        string CP { get; set; }
+        string TEL { get; set; }
+        string TIPO_PESC { get; set; }
+        string OCP_LABORAL { get; set; }
+        string CUERPO_DE_AGUA { get; set; }
+        string MATRICULA { get; set; }
+    }
+}
