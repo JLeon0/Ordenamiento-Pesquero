e9c5a84232e70ab699eb3830142339ada25213d0
ernesto <enesto_p_r@hotmail.com>
2018-02-26 13:17:12 -0700

    alv son un chingo a ver si no vale verga

---
+++ OrdenamientoPesquero/Logica/Procedimientos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrdenamientoPesquero.Clases_Entidad;
+using System.Data.SqlClient;
+
+namespace Logica
+{
+    public class Procedimientos
+    {
+        public void Registrar_Unidad(Unidad_Economica UE)
+        {
+
+        }
+        public void Actualizar_Unidad(Unidad_Economica UE)
+        {
+
+        }
+        public void Eliminar_Unidad(String RNPA)
+        {
+
+        }
+    }
+}
