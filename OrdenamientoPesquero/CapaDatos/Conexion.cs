1df7e9e6fc6fecedf321749357a18af09aab2b00
ernesto <enesto_p_r@hotmail.com>
2018-02-26 13:21:49 -0700

    kk

---
+++ OrdenamientoPesquero/CapaDatos/Conexion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+        public SqlConnection conexion;
+
+        //public static string obtenertconexion()
+        //{
+            //return Properties.Settings.Default.cadenaconexion;
+        //}
+        //public conexionBD()
+        //{
+          //  conexion = new SqlConnection(obtenertconexion());
+        //}
+
+        public void ejecutar(SqlCommand cmd)
+        {
+            conexion.Open();
+            cmd.Connection = conexion;
+            cmd.ExecuteNonQuery();
+            cmd.Connection.Close();
+            conexion.Close();
+        }
+
+    }
+}
