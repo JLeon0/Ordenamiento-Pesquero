9235e1a612a0d2f67221997bf3e6ab460cca0c28
ernesto <enesto_p_r@hotmail.com>
2018-04-02 01:20:36 -0600

    dd

--- OrdenamientoPesquero/OrdenamientoPesquero/Pantallas Registros/Pantalla_Registro_UnidadEconomica.Designer.cs
+++ OrdenamientoPesquero/OrdenamientoPesquero/Pantallas Registros/Pantalla_Registro_UnidadEconomica.Designer.cs
@@ -81,6 +81,7 @@
             this.Marcamotor = new System.Windows.Forms.DataGridViewComboBoxColumn();
             this.Potencia = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.nPer = new System.Windows.Forms.ComboBox();
             this.finVigenciaPer = new System.Windows.Forms.DateTimePicker();
             this.label38 = new System.Windows.Forms.Label();
             this.diaExpPer = new System.Windows.Forms.DateTimePicker();
@@ -232,7 +233,7 @@
             this.TotalPermisos = new System.Windows.Forms.Label();
             this.label59 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.nPer = new System.Windows.Forms.ComboBox();
+            this.BuscarPermiso = new System.Windows.Forms.PictureBox();
             this.gbOrgPes.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
@@ -273,6 +274,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pBReubicar)).BeginInit();
             this.Resumen.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BuscarPermiso)).BeginInit();
             this.SuspendLayout();
             // 
             // txtEstado
@@ -486,7 +488,7 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(83, 32);
+            this.label3.Location = new System.Drawing.Point(74, 32);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(401, 32);
             this.label3.TabIndex = 34;
@@ -921,6 +923,7 @@
             // 
             // groupBox3
             // 
+            this.groupBox3.Controls.Add(this.BuscarPermiso);
             this.groupBox3.Controls.Add(this.nPer);
             this.groupBox3.Controls.Add(this.finVigenciaPer);
             this.groupBox3.Controls.Add(this.label38);
@@ -942,6 +945,14 @@
             this.groupBox3.TabIndex = 67;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Datos del Permiso";
+            // 
+            // nPer
+            // 
+            this.nPer.FormattingEnabled = true;
+            this.nPer.Location = new System.Drawing.Point(123, 20);
+            this.nPer.Name = "nPer";
+            this.nPer.Size = new System.Drawing.Size(140, 26);
+            this.nPer.TabIndex = 70;
             // 
             // finVigenciaPer
             // 
@@ -2468,17 +2479,23 @@
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(775, 73);
+            this.pictureBox1.Size = new System.Drawing.Size(758, 73);
             this.pictureBox1.TabIndex = 30;
             this.pictureBox1.TabStop = false;
             // 
-            // nPer
+            // BuscarPermiso
             // 
-            this.nPer.FormattingEnabled = true;
-            this.nPer.Location = new System.Drawing.Point(123, 20);
-            this.nPer.Name = "nPer";
-            this.nPer.Size = new System.Drawing.Size(140, 26);
-            this.nPer.TabIndex = 70;
+            this.BuscarPermiso.BackColor = System.Drawing.Color.Transparent;
+            this.BuscarPermiso.BackgroundImage = global::OrdenamientoPesquero.Properties.Resources.lupa1;
+            this.BuscarPermiso.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BuscarPermiso.Image = global::OrdenamientoPesquero.Properties.Resources.lupa;
+            this.BuscarPermiso.Location = new System.Drawing.Point(278, 20);
+            this.BuscarPermiso.Name = "BuscarPermiso";
+            this.BuscarPermiso.Size = new System.Drawing.Size(37, 28);
+            this.BuscarPermiso.TabIndex = 115;
+            this.BuscarPermiso.TabStop = false;
+            this.toolTip1.SetToolTip(this.BuscarPermiso, "Buscar Unidad Economica");
+            this.BuscarPermiso.Click += new System.EventHandler(this.BuscarPermiso_Click);
             // 
             // Pantalla_Registro_UnidadEconomica
             // 
@@ -2557,6 +2574,7 @@
             this.Resumen.ResumeLayout(false);
             this.Resumen.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BuscarPermiso)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -2768,5 +2786,6 @@
         private System.Windows.Forms.TextBox MatriculaCertMat;
         private System.Windows.Forms.Label label15;
         private System.Windows.Forms.ComboBox nPer;
+        private System.Windows.Forms.PictureBox BuscarPermiso;
     }
 }
