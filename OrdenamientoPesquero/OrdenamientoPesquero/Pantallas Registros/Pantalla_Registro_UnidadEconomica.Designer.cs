56275123f7188b011116ad1044f48d9825eb9d08
ernesto <enesto_p_r@hotmail.com>
2018-06-14 09:53:01 -0600

    vxc

--- OrdenamientoPesquero/OrdenamientoPesquero/Pantallas Registros/Pantalla_Registro_UnidadEconomica.Designer.cs
+++ OrdenamientoPesquero/OrdenamientoPesquero/Pantallas Registros/Pantalla_Registro_UnidadEconomica.Designer.cs
@@ -264,7 +264,7 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(55, 44);
+            this.label3.Location = new System.Drawing.Point(46, 44);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(280, 22);
             this.label3.TabIndex = 34;
@@ -945,7 +945,7 @@
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Location = new System.Drawing.Point(75, 30);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(413, 54);
+            this.pictureBox1.Size = new System.Drawing.Size(396, 54);
             this.pictureBox1.TabIndex = 30;
             this.pictureBox1.TabStop = false;
             // 
