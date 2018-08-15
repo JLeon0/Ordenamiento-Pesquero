76ae022d51c09e8c23eb9154f74b3b04053211b5
ernesto <enesto_p_r@hotmail.com>
2018-08-15 14:20:56 -0600

    JJ

--- OrdenamientoPesquero/OrdenamientoPesquero/Properties/Settings.Designer.cs
+++ OrdenamientoPesquero/OrdenamientoPesquero/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace OrdenamientoPesquero.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,7 +26,7 @@ namespace OrdenamientoPesquero.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQLEXPRESS;Initial Catalog=OrdPesquero;Integrated Security=true;")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=OrdPesquero;Integrated Security=true;")]
         public string OrdPesqueroConnectionString {
             get {
                 return ((string)(this["OrdPesqueroConnectionString"]));
@@ -36,7 +36,7 @@ namespace OrdenamientoPesquero.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQLEXPRESS;Initial Catalog=OrdPesquero;Integrated Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=OrdPesquero;Integrated Security=True")]
         public string OrdPesqueroConnectionString3 {
             get {
                 return ((string)(this["OrdPesqueroConnectionString3"]));
@@ -46,7 +46,7 @@ namespace OrdenamientoPesquero.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=OrdPesquero;Integrated Security=true;")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQLEXPRESS;Initial Catalog=OrdPesquero;Integrated Security=true;")]
         public string OrdPesqueroConnectionString1 {
             get {
                 return ((string)(this["OrdPesqueroConnectionString1"]));
@@ -56,7 +56,7 @@ namespace OrdenamientoPesquero.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=OrdPesquero;Integrated Security=true;")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQLEXPRESS;Initial Catalog=OrdPesquero;Integrated Security=true;")]
         public string OrdPesqueroConnectionString2 {
             get {
                 return ((string)(this["OrdPesqueroConnectionString2"]));
