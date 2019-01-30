173e6d4f4f75e6e990f91d4726f9bb768fb247de
jesus leon <jesus_manuel4770@hotmail.com>
2019-01-30 11:28:49 -0700

    cac

--- OrdenamientoPesquero/OrdenamientoPesquero/Properties/Resources.Designer.cs
+++ OrdenamientoPesquero/OrdenamientoPesquero/Properties/Resources.Designer.cs
@@ -143,6 +143,16 @@ namespace OrdenamientoPesquero.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Firma {
+            get {
+                object obj = ResourceManager.GetObject("Firma", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap flechaabajo {
             get {
                 object obj = ResourceManager.GetObject("flechaabajo", resourceCulture);
