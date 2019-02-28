900754fa8270015d3ee2108962847978f5981479
jesus leon <jesus_manuel4770@hotmail.com>
2019-02-28 11:39:18 -0700

    aa

--- OrdenamientoPesquero/OrdenamientoPesquero/Pantallas Registros/Vistas.cs
+++ OrdenamientoPesquero/OrdenamientoPesquero/Pantallas Registros/Vistas.cs
@@ -623,7 +623,9 @@ namespace OrdenamientoPesquero.Pantallas_Registros
                         SoliApo[3] = new ReportParameter("Municipio", dt.Rows[0]["MUNICIO"].ToString());
                         SoliApo[4] = new ReportParameter("Localidad", dt.Rows[0]["LOCALIDAD"].ToString());
                         dt = proc.ObtenerUnaFederacion(rnpa);
-                        SoliApo[5] = new ReportParameter("Fed", dt.Rows[0]["NOMBRE"].ToString());
+                        if (dt.Rows[0]["NOMBRE"].ToString() == "")
+                        { SoliApo[5] = new ReportParameter("Fed", ""); }
+                        else { SoliApo[5] = new ReportParameter("Fed", dt.Rows[0]["NOMBRE"].ToString()); }
                     }
                     else
                     {
