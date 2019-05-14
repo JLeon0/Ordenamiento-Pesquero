56c9fcf7a858b76a1c6e438703bcd33849a8e7ac
ernesto <enesto_p_r@hotmail.com>
2019-05-14 08:20:18 -0600

    dfg

--- OrdenamientoPesquero/OrdenamientoPesquero/Program.cs
+++ OrdenamientoPesquero/OrdenamientoPesquero/Program.cs
@@ -17,12 +17,14 @@ namespace OrdenamientoPesquero
         [STAThread]
         static void Main()
         {
+
             bool instanceCountOne = false;
 
             using (Mutex mtex = new Mutex(true, "MyRunningApp", out instanceCountOne))
             {
                 if (instanceCountOne)
                 {
+
                     Application.EnableVisualStyles();
                     Application.SetCompatibleTextRenderingDefault(false);
                     Application.Run(new Login());
